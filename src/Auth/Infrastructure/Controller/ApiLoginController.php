8b2c0ea78e8635e09a87aea29f3ffaac773c49ad
Adam Zając <az@adamzajc.info>
2025-02-15 19:16:09 +0100

    json login

---
+++ src/Auth/Infrastructure/Controller/ApiLoginController.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Auth\Infrastructure\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class ApiLoginController extends AbstractController
+{
+    #[Route('/api/login', name: 'api_login', methods: ['POST'])]
+    public function index(): Response
+    {
+        $user = $this->getUser();
+        if (!$user) {
+            return $this->json(['message' => 'Unauthorized'], Response::HTTP_UNAUTHORIZED);
+        }
+
+        return $this->json(['message' => 'Welcome!']);
+    }
+}
---
+++ tests/Auth/Infrastructure/Controller/ApiLoginControllerTest.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Tests\Auth\Infrastructure\Controller;
+
+use App\Factory\UserFactory;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class ApiLoginControllerTest extends WebTestCase
+{
+    private KernelBrowser $client;
+
+    protected function setUp(): void
+    {
+        $this->client = static::createClient();
+
+        $passwordHasher = self::getContainer()->get(UserPasswordHasherInterface::class);
+
+        // Create a user with known credentials
+        $user = UserFactory::createOne([
+            'email' => 'user@example.com',
+            'password' => 'password',
+        ]);
+
+        $user->setPassword($passwordHasher->hashPassword($user, $user->getPassword()));
+    }
+
+    public function testSuccessfulLogin(): void
+    {
+        $this->client->request(
+            'POST',
+            '/api/login',
+            [],
+            [],
+            [
+                'CONTENT_TYPE' => 'application/json',
+            ],
+            json_encode([
+                'username' => 'user@example.com',
+                'password' => 'password',
+            ])
+        );
+
+        $this->assertResponseIsSuccessful();
+    }
+
+    public function testUnsuccessfulLogin(): void
+    {
+        $this->client->request(
+            'POST',
+            '/api/login',
+            [],
+            [],
+            [
+                'CONTENT_TYPE' => 'application/json',
+            ],
+            json_encode([
+                'username' => 'invalid@example.com',
+                'password' => 'invalid',
+            ])
+        );
+
+        $this->assertResponseStatusCodeSame(401);
+    }
+}
